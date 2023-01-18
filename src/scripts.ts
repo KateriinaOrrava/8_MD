319ca504f5083140f3a49dc9cbc41feba1c155b6
Katerina Orrava <98848233+KateriinaOrrava@users.noreply.github.com>
2023-01-18 11:14:01 +0200

    game changes

--- src/scripts.ts
+++ src/scripts.ts
@@ -5,22 +5,26 @@ const board = document.querySelector<HTMLDivElement | null>('.board-container');
 const stats = document.querySelector<HTMLDivElement | null>('.stats');
 const statsSeconds = document.querySelector<HTMLDivElement | null>('.stats___times--p');
 const statsMoves = document.querySelector<HTMLDivElement | null>('.stats__moves--p');
+// const statsPoints = document.querySelector<HTMLElement | null>('.stats__points--p');
 const cards = document.querySelectorAll<HTMLDivElement | null>('.card');
 const cardsValue = document.querySelectorAll<HTMLDivElement | null>('.card-value');
 const possValue = ['🍾', '🍾', '👆', '👆', '🤯', '🤯'];
 
-let clicked:string[]=[];
 let seconds = 0;
 let countClicks = 0;
 let nowValue = '';
-const prevValue = '';
+// let points = 0;
+
 // functions
 //
 function incrementSeconds() {
   seconds += 1;
   statsSeconds.innerHTML = `Your time: ${seconds} seconds`;
 }
-
+// function addPoints () {
+//   points += 1;
+//   statsPoints.innerHTML = `Points: ${points} seconds`;
+// }
 const shuffle = (array:string[]) => {
   let i = 0;
   let j = 0;
@@ -59,21 +63,61 @@ btn[0]?.addEventListener('click', () => {
 btn[1]?.addEventListener('click', () => {
   window.location.reload();
 });
-
 // this part for cards
+let nowId:string;
+let clicked:string[] = [];
+let idArr:string[] = [];
+
 [...cards].forEach((card) => {
   card.addEventListener('click', () => {
-    card.classList.toggle('is-flipped');
+    card.classList.add('is-flipped');
     clickCounter();
     nowValue = card.children[1].innerHTML;
-   // console.log(prevValue, nowValue);
-   clicked.push(nowValue);
-   console.log(clicked);
-  });
-});
+    clicked.push(nowValue);
+    nowId = card.id;
+    idArr.push(nowId);
+    const x = Number(idArr[0]);
+    const y = Number(idArr[1]);
+    if (clicked[0] === clicked[1] && clicked.length === 2 && x !== y) {
+      const keepCardsFlipped = () => {
+        cards[x].classList.add('is-flipped');
+        cards[y].classList.add('is-flipped');
+      };
+      // addPoints();
+      keepCardsFlipped();
+      clicked = [];
+      idArr = [];
+      console.log(clicked, idArr);
+    } else if (clicked[0] !== clicked[1] && clicked.length === 2 && x !== y) {
+      const flipCardBack = () => {
+        cards[x].classList.remove('is-flipped');
+        cards[y].classList.remove('is-flipped');
+      };
+      setInterval(flipCardBack, 1000);
+      idArr = []; clicked = [];
+      console.log(clicked, idArr);
+     
+    } else if (clicked[0] === clicked[1] && clicked.length === 2 && x === y) {
+      const flipCardBack = () => {
+        cards[x].classList.remove('is-flipped');
+        cards[y].classList.remove('is-flipped');
+      };
+      alert('you can not choose one element TWICE 🤦‍♀️');
+      setInterval(flipCardBack, 600);
+      idArr = [];
 
-[...cards].forEach((card) => {
-  card.addEventListener('mouseleave', () => {
-    card.classList.remove('is-flipped');
+      clicked = []; console.log(clicked, idArr);
+    } else {
+      console.log('choose on more');
+      console.log(clicked, idArr);
+    }
+  //   if (cards[0].classList.value.includes('is-flipped') === true
+  //  && cards[1].classList.value.includes('is-flipped')
+  //  && cards[2].classList.value.includes('is-flipped')
+  //  && cards[3].classList.value.includes('is-flipped')
+  //  && cards[4].classList.value.includes('is-flipped')
+  //  && cards[5].classList.value.includes('is-flipped')) {
+  //     alert('you flipped all cards');
+  //   }
   });
 });
